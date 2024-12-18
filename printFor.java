8a613c1275cbfeab3a189b53f8f14f930d4beb12
Haiboss <phonghai12@gmail.com>
2024-12-18 08:12:17 +0700

    Sửa đổi nội dung và cập nhật chương trình

---
+++ printFor.java
@@ -0,0 +1,8 @@
+public class printFor {
+    public static void main(String[] args) {
+        // Vòng lặp for hiển thị các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
